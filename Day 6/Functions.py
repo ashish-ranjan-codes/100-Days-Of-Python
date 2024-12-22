96df7ad50695ee843f9c3b8179c62314cc3ed291
ashish-ranjan-codes <findme.ashishranjan@gmail.com>
2024-12-22 14:49:37 +0530

    Day 6

---
+++ Day 6/Functions.py
@@ -0,0 +1,7 @@
+# let's learn about functions
+
+def my_function():
+    print("Hi, I am Ashish.")
+    return 0
+
+my_function()
---
+++ Day 6/WhileLoop.py
@@ -0,0 +1,7 @@
+# while loop in python
+
+n = int(input("Enter a number: "))
+
+while n>0:
+    print("Hello, ",n)
+    n -= 1
